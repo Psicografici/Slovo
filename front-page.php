847018c7f5a5def002f1f9670076ed71e0165f5e
Lorenzo De Angelis <psicografici@gmail.com>
2019-05-03 10:49:31 +0200

    Create front-page for homepage

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package slovo
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+?>
+
+
+<?php get_footer(); ?>
